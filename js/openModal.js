2ccef3821e341d4ab4804d98a0fad58e7c0935fa
Guseinov Rustam <rg20031991@gmail.com>
2022-09-12 21:07:31 +0300

    intermedia commit with popup

---
+++ js/openModal.js
@@ -0,0 +1,10 @@
+$('.article').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(800);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(800);
+    $('html').removeClass('no-scroll');
+});

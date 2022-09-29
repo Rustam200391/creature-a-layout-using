33a45cdf9fe4c7d6a4acd37f267077b17b570ca9
Guseinov Rustam <rg20031991@gmail.com>
2022-09-29 15:43:52 +0300

    comit without article in dataForm

--- js/gettingDataForm.js
+++ js/gettingDataForm.js
@@ -1,28 +1,4 @@
 
-
-//шаблон статьи
-// const articleSave =  `<section class="section1__item">
-//          <figure class="image-container">
-//              <section class="img-gradient"></section>
-//              <img src="${article.pic}" alt="image" class="pic" />
-            
-//          </figure>
-//          <article class="section1__item-content">
-//              <h2>${article.title}</h2>
-//              <p>${article.content}</p>
-//               <a href="../Article.htm" target="_blank" rel="noopener noreferrer">Читать далее</a> 
-//              <section class="section1__item-number">
-//                 <section class="number">300</section>
-//                 <section class="plus"></section>
-//                 <section class="minus"></section>
-//             </section>
-//          </article>
-//      </section>`;
-
-//пишем функцию что бы достать элементы шаблона
-
-
-
   function previewFile() {
     const preview = document.querySelector('img');
     const file = document.querySelector('input[type=file]').files[0];

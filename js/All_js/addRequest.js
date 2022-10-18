0fc05b60902a031069a51de17349d25e85647134
Guseinov Rustam <rg20031991@gmail.com>
2022-10-18 19:42:24 +0300

    intermedia commit with last changes

--- js/All_js/addRequest.js
+++ js/All_js/addRequest.js
@@ -28,43 +28,21 @@ request.then(response => response.json())
     const link = document.getElementsByTagName('a')[2];
     // находим карточку по тегу через doom
 
-
-    //предопределяем кнопку при клике на которую произойдет загрузка остальных 10 статей
-    const moreArticles = document.getElementById('more-article');
-
-    let addButton =  fetch('http://localhost:3000/article/?offset=2');//запрашиваем элементы с бэкенда, а   затем помещаем их в список статей, используя для этого шаблон статьи
-    addButton.then(response => response.json())
-    .then(data => {
-      data.forEach(moreArticle=>  
-        {
-        const buttonSave =  `<section class="section1__item">
-        <figure class="image-container">
-            <section class="img-gradient"></section>
-            <img src="${moreArticle.pic}" alt="image" />
-        </figure>
-        <article class="section1__item-content">
-            <h2>${moreArticle.title}</h2>
-            <p>${moreArticle.content}</p>
-             <a href="../Article.htm" target="_blank" rel="noopener noreferrer">Читать далее</a> 
-            <section class="section1__item-number">
-               <section class="number">300</section>
-               <section class="plus" data-change="+1"></section>
-               <section class="minus" data-change="-1"></section>
-           </section>
-        </article>
-    </section>`;
-      moreArticles.insertAdjacentHTML('beforeend', buttonSave) 
-      });
-      const pushButton = document.getElementsByTagName('input')[2];
-      // находим кнопку по тегу через doom
-  })
+        //предопределяем кнопку при клике на которую произойдет загрузка остальных 10 статей
+    // let addButton =  fetch('http://localhost:3000/article/?offset=2');//запрашиваем элементы с бэкенда, а   затем помещаем их в список статей, используя для этого шаблон статьи
 
 })
+  
   .catch(error => {//если метод вообще не может выполнить промис
     //  обработка ошибки
     console.log( error );
 });
 
+const moreArticles = document.getElementById('more-article');
+
+
+moreArticles.addEventListener('click',request());
+    // вешаеам обработчик событий при клике на мышью
 
 
 

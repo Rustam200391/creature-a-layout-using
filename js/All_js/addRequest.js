72668780ef444d756311d3325dab707a8cb64862
Guseinov Rustam <rg20031991@gmail.com>
2022-10-22 17:42:11 +0300

    Intermedia commit preparing for the task 33

--- js/All_js/addRequest.js
+++ js/All_js/addRequest.js
@@ -1,8 +1,8 @@
 
 const articleListParentNode = document.querySelector(".section1"); // находим через Doom добавить статью querySelector
 
-const request = () => {
-  fetch("http://localhost:3000/article/?offset=10") //запрашиваем элементы с бэкенда, а затем помещаем их в список статей, используя для этого шаблон статьи
+const request = (offset = 0) => {
+  fetch(`http://localhost:3000/article/?offset=${offset}`) //запрашиваем элементы с бэкенда, а затем помещаем их в список статей, используя для этого шаблон статьи
     .then((response) => response.json())
     .then((data) => {
       data.forEach((article) => {
@@ -18,8 +18,8 @@ const request = () => {
               <a href="../Article.htm" target="_blank" rel="noopener noreferrer">Читать далее</a> 
              <section class="section1__item-number">
                 <section class="number">300</section>
-                <section class="plus" data-change="+1"></section>
-                <section class="minus" data-change="-1"></section>
+                <section class="plus"></section>
+                <section class="minus"></section>
             </section>
          </article>
      </section>`;
@@ -31,6 +31,7 @@ const request = () => {
     });
 };
 
+request();
 const moreArticles = document.getElementById("more-article");
 
 
@@ -39,26 +40,23 @@ function examplesOffset() {
   console.log(offset);
   moreArticles.addEventListener('click', (e) => {
     offset +=10;
-    
-    // выводим в косоль \
     request(offset);
+  })
+    // выводим в косоль \
 }
 
+examplesOffset();
 
-// const request = () => {
-//   fetch("http://localhost:3000/article/?offset=10") 
-
-//   moreArticles.addEventListener(console.log(request)) {
-//     fetch("http://localhost:3000/article/?offset=10+10")
-//   }
-//   затем на обработчик событий вешается  событие, которое печатает в консоль текущий офсет, а затем увеличивает офсет на 10
-//   Т.е событие вешается на нашу кнопку
-
-// // // счетчик лайков
-
+// счетчик лайков
 // $('.section1__item-number').click(function(e) {
 //   const change = +e.target.dataset.change;
 //   if (change) {
 //     $('.number', this).text((i, text) => `${parseInt(text, 10) + change}`);
 //   }
 // });
+
+const plus = document.querySelector('plus');
+const minus = document.querySelector('minus');
+
+
+// plus.addEventListener

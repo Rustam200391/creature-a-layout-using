3dc9a0f3edaabaa10ad22e4368849c3ee3bdb9b2
Guseinov Rustam <rg20031991@gmail.com>
2022-10-23 17:55:16 +0300

    Intermedia commit with clicker and true links

--- js/All_js/addRequest.js
+++ js/All_js/addRequest.js
@@ -15,7 +15,7 @@ const request = (offset = 0) => {
          <article class="section1__item-content">
              <h2>${article.title}</h2>
              <p>${article.content}</p>
-              <a href="../Article.htm" target="_blank" rel="noopener noreferrer">Читать далее</a> 
+              <a href="..//..//html/Article.htm" target="_blank" rel="noopener noreferrer">Читать далее</a> 
              <section class="section1__item-number">
                 <section class="number">300</section>
                 <section class="plus" onClick="this.parentNode.querySelector('.number').innerText = parseInt(this.parentNode.querySelector('.number').innerText, 10) + 1;"></section>

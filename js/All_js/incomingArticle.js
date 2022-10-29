17bf3ac3cb1c74c107de2861ba06b4d1a238fde1
Guseinov Rustam <rg20031991@gmail.com>
2022-10-29 22:50:05 +0300

    Intermedia commit with addsLike

--- js/All_js/incomingArticle.js
+++ js/All_js/incomingArticle.js
@@ -4,31 +4,30 @@ import articleSave from "./articleSave.js";
 console.log("hello");
 const articleListParentNode = document.querySelector(".section1"); // находим через Doom добавить статью querySelector
 const request = (offset = 0) => {
-  console.log("ghbdtn");
   fetch(`http://localhost:3000/article/?offset=${offset}`) //запрашиваем элементы с бэкенда, а затем помещаем их в список статей, используя для этого шаблон статьи
     .then((response) => response.json())
     .then((data) => {
       data.forEach((article) => {
         const template = articleSave(article);
         console.log(template);
-        // // const plusBtn = cardCounter.querySelector(".plus");
-        // // const minusBtn = cardCounter.querySelector(".minus");
-        // // const counter = cardCounter.querySelector(".number");
-        // plusBtn.addEventListener("click", () => {
-        //   const count = Number(counter.textContent);
-        //   const body = {
-        //     likes: count + 1,
-        //   };
-        //   addLikes(article._id, body, counter);
-        // });
+        const plusBtn = document.querySelector(".plus");
+        const minusBtn = document.querySelector(".minus");
+        const counter = document.querySelector(".number");
+        plusBtn.addEventListener("click", () => {
+          const count = Number(counter.textContent);
+          const body = {
+            likes: count + 1,
+          };
+          addLikes(article._id, body, counter);
+        });
 
-        // minusBtn.addEventListener("click", () => {
-        //   const count = Number(counter.textContent);
-        //   const body = {
-        //     likes: count - 1,
-        //   };
-        //   addLikes(article._id, body, counter);
-        // });
+        minusBtn.addEventListener("click", () => {
+          const count = Number(counter.textContent);
+          const body = {
+            likes: count - 1,
+          };
+          addLikes(article._id, body, counter);
+        });
         articleListParentNode.appendChild(template);
       });
     })
